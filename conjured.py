a86e31f8766f1bef48fd0b76033d774cb635325a
Student <ziwei.polaris@gmail.com>
2019-08-29 23:00:35 +0100

    major conjured item refactor

--- conjured.py
+++ conjured.py
@@ -2,9 +2,5 @@ from item import *
 
 class Conjured(Item):
 
-    def update_quality(self):
-        if self.quality > 0:
-            if self.sell_in > 0:
-                self.quality = self.quality - 2
-            else:
-                self.quality = self.quality - 4
+    def decrement(self):
+        return 2 if self.sell_in > 0 else 4
