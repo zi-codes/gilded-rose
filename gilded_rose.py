e393845e4e5da409bebf6a79e28ba28b5ff866d8
Student <ziwei.polaris@gmail.com>
2019-08-29 20:42:04 +0100

    refactored sulfuras logic

--- gilded_rose.py
+++ gilded_rose.py
@@ -10,13 +10,10 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
-        self.update_sell_in_for_all()
         for item in self.items:
             if type(item.sell_in) is int:
+                self.update_sell_in(item)
                 item.update_quality()
 
-    def update_sell_in_for_all(self):
-        for item in self.items:
-            # this if statement will exclude sulfuras
-            if type(item.sell_in) is int:
-                item.sell_in = item.sell_in - 1
+    def update_sell_in(self,item):
+        item.sell_in = item.sell_in - 1
